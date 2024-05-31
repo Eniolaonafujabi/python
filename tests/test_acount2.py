b5347c04f35eab2960972ea88413b7a83a7443cf
Eniolaonafujabi <165755379+Eniolaonafujabi@users.noreply.github.com>
2024-05-31 06:49:15 +0100

    commit

---
+++ tests/test_acount2.py
@@ -0,0 +1,36 @@
+import unittest
+from acount import Account
+
+class MyTestCase(unittest.TestCase):
+    def test_that_i_can_check_balance(self):
+        eniolaAccount = Account()
+        balance: float = eniolaAccount.check_balance()
+        self.assertEqual(0, balance)
+
+    def test_that_i_can_increase_balance(self):
+        eniolaAccount = Account()
+        self.assertEqual(0,eniolaAccount.check_balance())
+        eniolaAccount.deposit(1700)
+        self.assertEqual(1700,eniolaAccount.check_balance())
+
+    def test_that_i_can_not_deposit_negative_amount(self):
+        eniolaAccount = Account()
+        self.assertEqual(0,eniolaAccount.check_balance())
+        eniolaAccount.deposit(-1700)
+        self.assertEqual(0,eniolaAccount.check_balance())
+
+    def test_that_i_can_withdraw_from_my_balance(self):
+        eniolaAccount = Account()
+        self.assertEqual(0,eniolaAccount.check_balance())
+        eniolaAccount.deposit(5000)
+        self.assertEqual(5000,eniolaAccount.check_balance())
+        eniolaAccount.withdraw(4000)
+        self.assertEqual(1000,eniolaAccount.check_balance())
+
+    def test_that_you_can_not_withdraw_more_than_your_amount(self):
+        eniolaAccount = Account()
+        self.assertEqual(0,eniolaAccount.check_balance())
+        eniolaAccount.deposit(5000)
+        self.assertEqual(5000,eniolaAccount.check_balance())
+        eniolaAccount.withdraw(-4000)
+        self.assertEqual(5000,eniolaAccount.check_balance())
