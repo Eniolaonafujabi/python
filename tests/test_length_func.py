25c1185192616852bada6031a007ba82fe6e4da1
Eniolaonafujabi <165755379+Eniolaonafujabi@users.noreply.github.com>
2024-06-05 16:09:26 +0100

    commit

--- tests/test_length_func.py
+++ tests/test_length_func.py
@@ -7,7 +7,7 @@ class MyTestCase(unittest.TestCase):
     def test_that_length_function_get_strings(self):
         self.assertEqual(6, get_the_length_of_your_word("eniola"))
 
-    def test_that_length_function_get_list(self):
+    def  test_that_length_function_get_list(self):
         self.assertEqual(0, get_the_length_of_your_word([]))
 
     def test_that_length_fuction_can_trough_exception(self):
